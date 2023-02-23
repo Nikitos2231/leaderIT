929aa213768dd821f09edc15fb095c2f7afe0998
Nikita <nik.porshennikov@mail.ru>
2023-02-23 15:29:47 +0700

    start structer added and connected depemdencies

---
+++ leaderIt_project/src/main/java/com/example/leaderIt_project/LeaderItProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaderIt_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderItProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderItProjectApplication.class, args);
+	}
+
+}
---
+++ leaderIt_project/src/test/java/com/example/leaderIt_project/LeaderItProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaderIt_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderItProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
